a77c6e172770ffb3b2254286ab567582ccb54eed
denizmatar <denizmatar1@gmail.com>
2021-03-06 17:46:31 +0300

    update

--- main.py
+++ main.py
@@ -10,6 +10,28 @@ TAKER_FEE = 0.002
 CSV_PATH = "/Users/denizmatar/PycharmProjects/crypto-arbitrage/data.csv"
 FIELD_NAMES = ['time', 'pair', 'profit', 'buy_exchange', 'sell_exchange', 'slippage', 'maker_fee', 'taker_fee']
 
+ask_prices_dict = {
+    'last_update': None,
+    'BTC/USDT': {'binance': None,
+                 'kraken': None,
+                 'poloniex': None},
+    'ATOM/BTC': {'binance': None,
+                 'kraken': None,
+                 'poloniex': None},
+    'ETH/BTC': {'binance': None,
+                'kraken': None,
+                'poloniex': None}}
+bid_prices_dict = {
+    'last_update': None,
+    'BTC/USDT': {'binance': None,
+                 'kraken': None,
+                 'poloniex': None},
+    'ATOM/BTC': {'binance': None,
+                 'kraken': None,
+                 'poloniex': None},
+    'ETH/BTC': {'binance': None,
+                'kraken': None,
+                'poloniex': None}}
 
 def float_formatter(flt):
     return "{:.2f}".format(flt)
@@ -20,16 +42,17 @@ def float_formatter(flt):
 
 async def exchange_loop(exchange, symbol):
     global orderbook
-    print(f'Starting the {exchange.id.upper()} exchange loop with {symbol}')
-    # ask_prices_dict = {}
-    # bid_prices_dict = {}
+    print(f'Starting the {exchange.id.upper()} exchange loop for {symbol}')
+
     while True:
         try:
             orderbook = await exchange.fetch_order_book(symbol)
             print(f"best ask price for {symbol} in {exchange.id} is {orderbook['asks'][0]}")
             print(f"best bid price for {symbol} in {exchange.id} is {orderbook['bids'][0]}")
-            # ask_prices_dict[symbol] = {exchange.id: orderbook['asks'][0][0]}
-            # bid_prices_dict[symbol] = {exchange.id: orderbook['bids'][0][0]}
+            ask_prices_dict[symbol].update({exchange.id: orderbook['asks'][0][0]})
+            bid_prices_dict[symbol].update({exchange.id: orderbook['bids'][0][0]})
+            print(ask_prices_dict)
+            print(bid_prices_dict)
         except Exception as e:
             print(str(e))
             break
@@ -40,21 +63,20 @@ async def exchange_loop(exchange, symbol):
 
 async def symbol_loop(asyncio_loop, symbol, exchange_ids):
     print(f"Starting the {symbol} symbol loop for {exchange_ids}")
-    ask_prices_dict = {}
-    bid_prices_dict = {}
+    # ask_prices_dict = {}
+    # bid_prices_dict = {}
     exchanges = [getattr(ccxt, exchange_id)({
         'enableRateLimit': True,
         'asyncio_loop': asyncio_loop
     }) for exchange_id in exchange_ids]
-    print(exchanges)
 
     loops = [exchange_loop(exchange, symbol) for exchange in exchanges]
-    print(loops)
     await gather(*loops)
-    ask_prices_dict[symbol] = {exchange.id: orderbook['asks'][0][0]}
-    bid_prices_dict[symbol] = {exchange.id: orderbook['bids'][0][0]}
-    print(ask_prices_dict)
-    print(bid_prices_dict)
+
+    # ask_prices_dict[symbol] = {exchange.id: orderbook['asks'][0][0]}
+    # bid_prices_dict[symbol] = {exchange.id: orderbook['bids'][0][0]}
+    # print(ask_prices_dict)
+    # print(bid_prices_dict)
     await exchange.close()
 
 
@@ -67,9 +89,9 @@ async def main(asyncio_loop):
     }
 
     pairs = {
-        'BTC/USDT': ['binance', 'kraken', 'bitfinex'],
-        'ATOM/USDT': ['binance', 'bitfinex'],
-        'ETH/USDT': ['binance', 'kraken', 'bitfinex']
+        'BTC/USDT': ['binance', 'kraken', 'poloniex'],
+        'ATOM/BTC': ['binance', 'kraken', 'poloniex'],
+        'ETH/BTC': ['binance', 'kraken', 'poloniex']
     }
     loops = [symbol_loop(asyncio_loop, symbol, exchange_ids) for symbol, exchange_ids in pairs.items()]
     await gather(*loops)
